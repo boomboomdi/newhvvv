8463fa42f271b22836143f98f6d811c4ae513e46
bll <757633801@qq.com>
2022-05-08 05:28:24 +0800

    order_limit_time

--- application/shell/Timecheckorder.php
+++ application/shell/Timecheckorder.php
@@ -34,7 +34,7 @@ class Timecheckorder extends Command
             $orderData = $orderModel
                 ->where('order_status', '=', 4)
                 ->where('next_check_time', '<', time())
-                ->where('check_status', '<>', 0)
+                ->where('check_status', '=', 0)
                 ->where('check_times', '<', 5)
                 ->select();
             logs(json_encode(['orderData' => $orderData,
