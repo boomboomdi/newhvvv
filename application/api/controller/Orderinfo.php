e031d9622dbbd139f04a9cae33aa128c8920ee13
bll <757633801@qq.com>
2022-05-10 03:01:12 +0800

    pcntl_fork

--- application/api/controller/Orderinfo.php
+++ application/api/controller/Orderinfo.php
@@ -72,7 +72,6 @@ class Orderinfo extends Controller
             $insertOrderData['add_time'] = time();  //入库时间
             $insertOrderData['notify_url'] = $message['notify_url']; //下单回调地址 notify url
 
-
             $orderModel = new OrderModel();
             $createOrderOne = $orderModel->addOrder($insertOrderData);
             if (!isset($createOrderOne['code']) || $createOrderOne['code'] != '0') {
