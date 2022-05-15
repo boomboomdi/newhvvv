88cc402d61ea12e7b8288ca5035481997557e5be
bll <757633801@qq.com>
2022-05-15 23:01:33 +0800

    uniqid

--- application/api/controller/Orderinfo.php
+++ application/api/controller/Orderinfo.php
@@ -57,7 +57,7 @@ class Orderinfo extends Controller
             //$user_id = $message['user_id'];  //用户标识
             // 根据user_id  未付款次数 限制下单 end
 
-            $orderMe = uuidA();
+            $orderMe = md5(uniqid().uuidA());
 
             $orderFind = $db::table('bsa_order')->where('order_me', '=', $orderMe)->find();
             if (!empty($orderFind)) {
