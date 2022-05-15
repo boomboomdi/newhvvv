5332d4beb67fa9c112cbcef2cd13ea9642f65818
bll <757633801@qq.com>
2022-05-15 17:09:29 +0800

    Statistics/order

--- application/admin/controller/Statistics.php
+++ application/admin/controller/Statistics.php
@@ -132,4 +132,116 @@ class Statistics extends Base
 
         return $this->fetch();
     }
+
+
+    //
+    public function order()
+    {
+        if (request()->isAjax()) {
+
+            $limit = input('param.limit');
+            $merchantSign = input('param.merchant_sign'); //核销名称
+
+            $where = [];
+            if (empty($merchantSign)) {
+                return json(['code' => 0, 'msg' => 'ok', 'count' => 0, 'data' => []]);
+            }
+
+            $startTime = input('param.startTime'); //开始时间
+            if (empty($startTime)) {
+                $startTime = date("Y-m-d", time());
+            }
+            $where[] = ['merchant_sign', '=', $merchantSign];
+            $where[] = ['pay_time', '>', strtotime($startTime)];
+            $where[] = ['pay_time', '<', (strtotime($startTime) + 86400)];
+//            if (!empty($writeOffSign)) {
+//                $where[] = ['write_off_sign', '=', $writeOffSign . '%'];
+//            }
+//            $writeOffModel = new WriteoffModel();
+//            $writeOff = $writeOffModel->getWriteOffBySign($writeOffSign);
+
+//            if (empty($writeOff)) {
+//                return json(['code' => -1, 'msg' => '核销商标识输入有误！', 'count' => 0, 'data' => []]);
+//            }
+            $orderModel = new OrderModel();
+//            $orderHxModel = new Orderhexiaomodel();
+            $list = $orderModel->field("amount,merchant_sign")
+                ->where($where)
+                ->group("amount")
+                ->order("amount desc")
+//                ->select();
+//                ->limit($limit)
+                ->paginate($limit);
+//            $totalCount = $list->count();
+//            $res = $this->field($prefix . 'order.*')
+//                ->where($where)
+//                ->order('id', 'desc')
+//                ->paginate($limit);
+//            $list = $orderModel->getStatistics($limit, $where);
+            if (0 < count($list)) {
+                $data = $list;
+                $orderTotalNum = 0; //总推单数量
+                $totalOrderAmount = 0; //推单总额
+                $totalPayOrderAmount = 0; //总支付金额
+                $totalPayOrderAmountNum = 0; //总支付数量
+                $k = 0;
+                foreach ($data as $key => $vo) {
+                    $k++;
+//                    $data[$key]['order_amount'] = $vo['order_amount'];
+                    //推单数量（每个金额）
+                    $data[$key]['orderTotalNum'] = $orderModel
+                        ->where($where)
+                        ->where("amount", "=", $vo['amount'])
+                        ->count();
+                    $orderTotalNum += $data[$key]['orderTotalNum'];
+
+                    //推单总额（每个金额）
+                    $data[$key]['totalOrderAmount'] = $orderModel
+                        ->where($where)
+                        ->field("SUM(amount) as totalOrderAmount")
+                        ->where("amount", "=", $vo['amount'])
+                        ->find()['totalOrderAmount'];
+                    $totalOrderAmount += $data[$key]['totalOrderAmount'];
+
+                    //总支付数量（每个金额）
+                    $data[$key]['totalPayOrderAmountNum'] = $orderModel
+                        ->where($where)
+                        ->where("amount", "=", $vo['amount'])
+                        ->count();
+                    $totalPayOrderAmountNum += $data[$key]['totalPayOrderAmountNum'];
+                    //总支付金额（每个金额）
+                    $data[$key]['totalPayOrderAmount'] = $orderModel
+                        ->field("SUM(actual_amount) as totalPayOrderAmount")
+                        ->where($where)
+                        ->where("amount", "=", $vo['amount'])
+                        ->find()['totalPayOrderAmount'];
+                    $totalPayOrderAmount += $data[$key]['totalPayOrderAmount'];
+
+                }
+                $total['amount'] = '总统计';
+                $total['merchant_sign'] = $merchantSign;
+                $total['orderTotalNum'] = $orderTotalNum;
+                $total['totalOrderAmount'] = $totalOrderAmount;
+                $total['totalPayOrderAmount'] = $totalPayOrderAmount;
+                $total['totalPayOrderAmountNum'] = $totalPayOrderAmountNum;
+
+                $data[] = $total;
+                $list = $data;
+//                var_dump(count($list));
+////                exit;
+//                $orderTotalNum = 0; //总推单数量
+//                $totalOrderAmount = 0; //推单总额
+//                $totalPayOrderAmount = 0; //总支付金额
+//                $totalPayOrderAmountNum = 0; //总支付数量
+                $list = $data;
+//                var_dump($list->all());exit;
+//                return json(['code' => 0, 'msg' => 'ok', 'count' => $list['data']->total(), 'data' => $list['data']->all()]);
+                return json(['code' => 0, 'msg' => 'ok', 'count' => $list->total(), 'data' => $list->all()]);
+            }
+
+            return json(['code' => 0, 'msg' => 'ok', 'count' => 0, 'data' => []]);
+        }
+
+        return $this->fetch();
+    }
 }
