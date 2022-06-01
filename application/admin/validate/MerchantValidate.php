0d3c0cd52b4e52336147aea570b8169933e2590e
bll <757633801@qq.com>
2022-06-01 08:38:53 +0800

    merchant

--- application/admin/validate/MerchantValidate.php
+++ application/admin/validate/MerchantValidate.php
@@ -15,13 +15,13 @@ class MerchantValidate extends Validate
     protected $rule =   [
         'merchant_name'  => 'require',
         'merchant_sign'   => 'require',
-        'merchant_username'  => 'require',
+//        'merchant_username'  => 'require',
 //        'merchant_password'   => 'require',
 //        'merchant_validate_password'   => 'require'   //敏感操作验证密码
     ];
 
     protected $message  =   [
-        'merchant_name.require' => '商户名称不能为空',
+//        'merchant_name.require' => '商户名称不能为空',
         'merchant_sign.require'   => '商户标识不能为空',
         'merchant_username.require'   => '商户登录名不能为空',
 //        'merchant_password.require'   => '商户密码不能为空',
