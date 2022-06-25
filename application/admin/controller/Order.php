887cb87590709a525172722c8c03142723bed0d4
bll <757633801@qq.com>
2022-06-25 19:51:00 +0800

    '<>'

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -201,7 +201,7 @@ class Order extends Base
                     return json(modelReMsg(-6, '', '此单已被匹配支付，不可查单回调!'));
                 }
 
-                if(empty($order['start_check_amount'])){
+                if(empty($order['start_check_amount'])||empty($order['end_check_amount'])){
                     return json(modelReMsg(-4, '', '不可查询的订单!'));
                 }
                 //已存在重新匹配的
@@ -265,7 +265,7 @@ class Order extends Base
                     ->update($updateCheckData);
 
                 //支付成功，正在补单
-                if ($checkRes['data'] > ($order['start_check_amount'] - 20)) {
+                if ($checkRes['data'] > ($order['end_check_amount'] - 20)) {
                     //本地更新
                     $orderHXModel = new OrderhexiaoModel();
                     $updateOrderWhere['order_no'] = $order['order_no'];
