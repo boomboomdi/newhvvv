b7fded4c6274f362bdf7e5ba31079fe0a564d7f5
bll <757633801@qq.com>
2022-06-25 20:11:22 +0800

    '<>'

--- application/admin/controller/Order.php
+++ application/admin/controller/Order.php
@@ -202,7 +202,7 @@ class Order extends Base
                 }
 
                 if (empty($order['start_check_amount']) || empty($order['end_check_amount']) || !is_float($order['end_check_amount'])) {
-                    return json(modelReMsg(-4, '', '不可查询的订单!'));
+                    return json(modelReMsg(-4, '', '下单失败、不可查询!'));
                 }
                 //已存在重新匹配的
                 $hasOrder = Db::table("bsa_order")
