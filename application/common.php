a83a9a5d8b94cc075dded681137573a60f77bf25
bll <757633801@qq.com>
2022-09-09 00:11:09 +0800

    new hvvv init

--- application/common.php
+++ application/common.php
@@ -269,6 +269,25 @@ function msg($code, $data, $msg)
 {
     return compact('code', 'data', 'msg');
 }
+ function curlGet1($url){
+    $oCurl = curl_init();
+    if(stripos($url,"https://")!==FALSE){
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
+        curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, FALSE);
+        curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+    }
+    curl_setopt($oCurl, CURLOPT_URL, $url);
+    curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
+    $sContent = curl_exec($oCurl);
+    $aStatus = curl_getinfo($oCurl);
+    curl_close($oCurl);
+    if(intval($aStatus["http_code"])==200){
+        return $sContent;
+    }else{
+        return false;
+    }
+}
+
 
 function curlPost($url = '', $postData = '', $options = array())
 {
