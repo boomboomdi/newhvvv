6c72d94db29b455cad325e3ebf6fb13005e660ad
bll <757633801@qq.com>
2022-09-08 22:51:50 +0800

    new hvvv init

--- application/api/controller/Order.php
+++ application/api/controller/Order.php
@@ -22,7 +22,6 @@ class Order extends Controller{
         $checkParam['phone'] = "13782396066";
         $checkParam['amount'] = 100;
         $checkParam['order_me'] = 'sadadsadadasdsaddsda';
-        $orderHeXModel = new OrderhexiaoModel();
         $url = "http://119.91.82.145/api/createOrder?token=47a4f42371348b1dad5c813eb89e4db7&phone=13782396069&channel=swye&pay_type=微信&amount=100&out_trade_no=88888888&lock_time=10&callback_url=http://119.91.82.145/api/callback";
         $res = curlGet($url);
         var_dump($res);exit;
--- application/common.php
+++ application/common.php
@@ -371,20 +371,17 @@ function curlPostJsonNew($url = '', $postData = '', $options = array())
 
 function curlGet($url=""){
 
-    $curl = curl_init();
+    $ch = curl_init();
+    //设置选项，包括URL
+    curl_setopt($ch, CURLOPT_URL, $url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+    curl_setopt($ch, CURLOPT_HEADER, 0);
 
-    curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($curl, CURLOPT_TIMEOUT, 500);
-    // 为保证第三方服务器与微信服务器之间数据传输的安全性，所有微信接口采用https方式调用，必须使用下面2行代码打开ssl安全校验。
-    // 如果在部署过程中代码在此处验证失败，请到 http://curl.haxx.se/ca/cacert.pem 下载新的证书判别文件。
-    // curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
-    // curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
-    curl_setopt($curl, CURLOPT_URL, $url);
-
-    $res = curl_exec($curl);
-    curl_close($curl);
-
-    return $res;
+    //执行并获取HTML文档内容
+    $output = curl_exec($ch);
+    //释放curl句柄
+    curl_close($ch);
+    return $output;
 }
 
 function curlPostJson($url = '', $postData = '', $options = array())
