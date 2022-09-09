98c72cd323c864a1b593ddf8d4230c1af8f9a302
bll <757633801@qq.com>
2022-09-09 17:47:21 +0800

    new hvvv init

--- application/common.php
+++ application/common.php
@@ -269,24 +269,64 @@ function msg($code, $data, $msg)
 {
     return compact('code', 'data', 'msg');
 }
- function curlGet1($url){
-    $oCurl = curl_init();
-    if(stripos($url,"https://")!==FALSE){
-        curl_setopt($oCurl, CURLOPT_SSL_VERIFYPEER, FALSE);
-        curl_setopt($oCurl, CURLOPT_SSL_VERIFYHOST, FALSE);
-        curl_setopt($oCurl, CURLOPT_SSLVERSION, 1); //CURL_SSLVERSION_TLSv1
+
+function curlGet1($url, $type, $data = false, &$err_msg = null, $timeout = 20, $cert_info = array())
+{
+    $type = strtoupper($type);
+    if ($type == 'GET' && is_array($data)) {
+        $data = http_build_query($data);
     }
-    curl_setopt($oCurl, CURLOPT_URL, $url);
-    curl_setopt($oCurl, CURLOPT_RETURNTRANSFER, 1 );
-    $sContent = curl_exec($oCurl);
-    var_dump($sContent);exit;
-    $aStatus = curl_getinfo($oCurl);
-    curl_close($oCurl);
-    if(intval($aStatus["http_code"])==200){
-        return $sContent;
-    }else{
-        return false;
+
+    $option = array();
+
+    if ($type == 'POST') {
+        $option[CURLOPT_POST] = 1;
     }
+    if ($data) {
+        if ($type == 'POST') {
+            $option[CURLOPT_POSTFIELDS] = $data;
+        } elseif ($type == 'GET') {
+            $url = strpos($url, '?') !== false ? $url . '&' . $data : $url . '?' . $data;
+        }
+    }
+
+    $option[CURLOPT_URL] = $url;
+    $option[CURLOPT_FOLLOWLOCATION] = TRUE;
+    $option[CURLOPT_MAXREDIRS] = 4;
+    $option[CURLOPT_RETURNTRANSFER] = TRUE;
+    $option[CURLOPT_TIMEOUT] = $timeout;
+
+    //设置证书信息
+    if (!empty($cert_info) && !empty($cert_info['cert_file'])) {
+        $option[CURLOPT_SSLCERT] = $cert_info['cert_file'];
+        $option[CURLOPT_SSLCERTPASSWD] = $cert_info['cert_pass'];
+        $option[CURLOPT_SSLCERTTYPE] = $cert_info['cert_type'];
+    }
+
+    //设置CA
+    if (!empty($cert_info['ca_file'])) {
+        // 对认证证书来源的检查，0表示阻止对证书的合法性的检查。1需要设置CURLOPT_CAINFO
+        $option[CURLOPT_SSL_VERIFYPEER] = 1;
+        $option[CURLOPT_CAINFO] = $cert_info['ca_file'];
+    } else {
+        // 对认证证书来源的检查，0表示阻止对证书的合法性的检查。1需要设置CURLOPT_CAINFO
+        $option[CURLOPT_SSL_VERIFYPEER] = 0;
+    }
+
+    $ch = curl_init();
+    curl_setopt_array($ch, $option);
+    $response = curl_exec($ch);
+    $curl_no = curl_errno($ch);
+    $curl_err = curl_error($ch);
+    curl_close($ch);
+
+    // error_log
+    if ($curl_no > 0) {
+        if ($err_msg !== null) {
+            $err_msg = '(' . $curl_no . ')' . $curl_err;
+        }
+    }
+    return $response;
 }
 
 
