26c4c873be7b45c23c4a4b06d78eb333c352d823
bll <757633801@qq.com>
2022-09-10 18:32:58 +0800

    new hvvv yinHeBalance

--- application/api/controller/Ceshi.php
+++ application/api/controller/Ceshi.php
@@ -132,7 +132,8 @@ class Ceshi extends Controller
         $out_trade_no = 100;
         $res = $spendBalance->yinHeBalance($account, $amount, $orderNo, '88888888', $pay_type);
         $data = json_decode($res, true);
-        $data = json_decode($data['data'], true);
+        $data =$data['data'];
+//        $data = json_decode($data['data'], true);
 
         var_dump($data);
         exit;
